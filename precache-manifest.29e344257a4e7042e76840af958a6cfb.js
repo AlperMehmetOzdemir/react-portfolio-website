0e6f3bb97b151d9df0e5293ff2d932e74708f72a
AlperMehmetOzdemir <32219367+AlperMehmetOzdemir@users.noreply.github.com>
2020-07-01 18:47:15 +0300

    Updates

---
+++ precache-manifest.29e344257a4e7042e76840af958a6cfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143d4aa625b876e9b609b9097e37e8b7",
+    "url": "/react-portfolio-website/index.html"
+  },
+  {
+    "revision": "5c2619787e8798905123",
+    "url": "/react-portfolio-website/static/css/main.88ef062a.chunk.css"
+  },
+  {
+    "revision": "91ddd959d1f2144dd3f6",
+    "url": "/react-portfolio-website/static/js/2.bfc79442.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio-website/static/js/2.bfc79442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2619787e8798905123",
+    "url": "/react-portfolio-website/static/js/main.51e63e14.chunk.js"
+  },
+  {
+    "revision": "3478caa6103f86f17f6f",
+    "url": "/react-portfolio-website/static/js/runtime-main.85d82834.js"
+  },
+  {
+    "revision": "aa2c2ca8a8f59817c0dc8837d29c865a",
+    "url": "/react-portfolio-website/static/media/ColorShooter.aa2c2ca8.jpg"
+  },
+  {
+    "revision": "a10981df02855a585a704c171159d346",
+    "url": "/react-portfolio-website/static/media/ReactPortfolio.a10981df.jpg"
+  }
+]);
