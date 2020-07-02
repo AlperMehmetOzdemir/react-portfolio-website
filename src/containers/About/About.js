f313d1876cf75bd8a8537c2c9654e43dead64921
AlperMehmetOzdemir <32219367+AlperMehmetOzdemir@users.noreply.github.com>
2020-07-02 10:02:15 +0300

    Updated favicons, title and added photo to about me

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import classes from "./About.module.css";
+import PortraitPhoto from "../../assets/images/portraitPhoto.jpg";
 
 const about = (props) => {
   return (
@@ -9,16 +10,17 @@ const about = (props) => {
         <span className={classes.Underline}>About Me</span>
       </h2>
       <div className={classes.Content}>
-        <p>
+        <p className={classes.Text1}>
           Hello! I'm Mehmet, a 4th year computer engineer student in Bilkent
           University.
         </p>
-        <p>
+        <p className={classes.Text2}>
           I enjoy learning new technologies and creating / analyzing apps and
           video games that have innovative and intuitive design. I aim to keep
           improving myself as a computer engineer with a focus on web
           development and build products that others will enjoy using.
         </p>
+        <img className={classes.Img} src={PortraitPhoto} />
       </div>
     </section>
   );
